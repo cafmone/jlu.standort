b1ab64b4361442c588575bf1d1f7ffa56a8857b9
akuballa <akuballa@users.noreply.github.com>
2021-05-24 14:33:07 +0200

    adding link import

--- class/plugins/jlu.standort/class/jlu.standort.import.links.class.php
+++ class/plugins/jlu.standort/class/jlu.standort.import.links.class.php
@@ -48,42 +48,125 @@ class jlu_standort_import_links
 	function action($action = null) {
 		$file = 'jlu.standort.import.links.ini';
 		$result = $this->db->select($file,'*');
+
 		if(is_array($result)) {
-			### TODO do something
-			echo 'Import Links';
 
-		}
-	}
+			$output  = array();
 
-	//------------------------------------------------
-	/**
-	 * Sort array [ids] by key [sort]
-	 *
-	 * @access protected
-	 * @param array $ids
-	 * @param string $sort
-	 * @param enum $order [ASC/DESC]
-	 * @return array
-	 */
-	//------------------------------------------------
-	function __sort($ids, $sort, $order = '') {
-		if($order !== '') {
-			if($order == 'ASC') $sort_order = SORT_ASC;
-			if($order == 'DESC') $sort_order = SORT_DESC;
-		} else {
-			$sort_order = SORT_ASC;
-		}
-		$column = array();
-		reset($ids);
-		foreach($ids as $val) {
-			if(isset($val[$sort])) {
-				$column[] = $val[$sort];
+			foreach($result as $ection => $r) {
+
+				// handle import.ini values
+				$cols = array();
+
+				// check key id
+				if(isset($r['id']) && $r['id'] !== '') {
+					$key = $r['id'];
+					$matches = @preg_match('~^[A-Z]+$~', $key);
+					if(!$matches) {
+						echo 'ERROR: misspelled key id "'.$key.'" in '.$file.' section ['.$ection.']';
+						$this->response->html->help($r);
+						exit();
+					} else {
+						$cols['id'] = $key;
+					}
+				} else {
+					echo 'ERROR: Missing or empty key id in '.$file.' section ['.$ection.']';
+					$this->response->html->help($r);
+					exit();
+				}
+				// check key zlisurl
+				if(isset($r['zlisurl']) && $r['zlisurl'] !== '') {
+					$key = $r['zlisurl'];
+					$matches = @preg_match('~^[A-Z]+$~', $key);
+					if(!$matches) {
+						echo 'ERROR: misspelled key zlisurl "'.$key.'" in '.$file.' section ['.$ection.']';
+						$this->response->html->help($r);
+						exit();
+					} else {
+						$cols['zlisurl'] = $key;
+					}
+				} else {
+					$cols['zlisurl'] = '';
+					#echo 'ERROR: Missing or empty key zlisurl in '.$file.' section ['.$ection.']';
+					#$this->response->html->help($r);
+					#exit();
+				}
+				// check key jlucourl
+				if(isset($r['jlucourl']) && $r['jlucourl'] !== '') {
+					$key = $r['jlucourl'];
+					$matches = @preg_match('~^[A-Z]+$~', $key);
+					if(!$matches) {
+						echo 'ERROR: misspelled key jlucourl "'.$key.'" in '.$file.' section ['.$ection.']';
+						$this->response->html->help($r);
+						exit();
+					} else {
+						$cols['jlucourl'] = $key;
+					}
+				} else {
+					$cols['jlucourl'] = '';
+					// TODO not needed ?
+					// echo 'ERROR: Missing or empty key zlisurl in '.$file.' section ['.$ection.']';
+					// $this->response->html->help($r);
+					// exit();
+				}
+				// check key file
+				if(!isset($r['file']) || $r['file'] === '') {
+					echo 'ERROR: Missing or empty key file in '.$file.' section ['.$ection.']';
+					exit();
+				}
+				// check key sheet
+				if(!isset($r['sheet']) || $r['sheet'] === '') {
+					echo 'ERROR: Missing or empty key sheet in '.$file.' section ['.$ection.']';
+					exit();
+				}
+				// check key offset
+				if(!isset($r['offset']) || $r['offset'] === '') {
+					echo 'ERROR: Missing or empty key offset in '.$file.' section ['.$ection.']';
+					exit();
+				}
+
+				// parse xlsx file
+				$path = $this->PROFILESDIR.'import/'.$r['file'];
+
+				$xlsx = new file_xlsx($this->file, $this->response->html);
+				$xlsx->sheet = $r['sheet'];
+				$xlsx->row   = $r['offset'];
+				$xlsx->cols  = $cols;
+
+				$content = $xlsx->parse($path);
+				if(is_array($content)) {
+					foreach($content as $k => $c) {
+
+						// check values not empty
+						if( $c[$cols['zlisurl']] === '' && $c[$cols['jlucourl']] === '') {
+							echo 'NOTICE: Empty column(s) in file '.$r['file'].'. Skipping row '.$k.'.<br>';
+						} else {
+							// escape content (xss)
+							$id = htmlEntities($c[$cols['id']], ENT_QUOTES);
+							$c[$cols['zlisurl']] !== ''  ? $output[$id]['zlisurl'] = $c[$cols['zlisurl']] : null;
+							$c[$cols['jlucourl']] !== '' ? $output[$id]['jlucourl'] = $c[$cols['jlucourl']] : null;
+						}
+					}
+				} else {
+					echo 'ERROR: '.$content.'.<br>';
+					return;
+				}
 			}
+
+			// handle cache
+			$path = $this->response->html->thisdir.'cache/';
+			if($this->file->is_writeable($path)) {
+				$error = $this->file->mkfile($path.'links.json', json_encode($output),'w+',true);
+				if($error === '') {
+						echo 'Import successful.';
+				} else {
+					echo $error;
+				}
+			} else {
+				echo 'ERROR: Folder '.$path.' is readonly.';
+			}
+
 		}
-		if(count($ids) === count($column)) {
-			array_multisort($column, $sort_order, $ids);
-		}
-		return $ids;
 	}
 
 }
